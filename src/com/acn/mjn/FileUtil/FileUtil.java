8a383c00170241729c9f3731faf66821238e92c1
shanjun <739051127@qq.com>
2016-06-27 23:32:24 +0800

    FirstUpload
    Upload from Local GUI Client to Cloud

---
+++ src/com/acn/mjn/FileUtil/FileUtil.java
@@ -0,0 +1,28 @@
+package com.acn.mjn.FileUtil;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class FileUtil {
+	public static String currentWorkDir = System.getProperty("user.dir")+"\\";
+	
+	public static String leftPad(String str, int length, char ch) {
+		if(str.length() >= length) {
+			return str;
+		}
+		
+		char[] chs = new char[length];
+		Arrays.fill(chs, ch);
+		char[] src = str.toCharArray();
+		return null;
+	}
+	
+	public List<String> splitBySize(String fileName,int byteSize) {
+		List<String> parts = new ArrayList<String>();
+		File file = new File(fileName);
+		int count = (int)Math.ceil(file.length() / (double))
+		//int countLen = (count+"").length();
+		return null;
+	}
+}
